f6da7f4f78c00fa7d26348b36ba4c25776d2b680
chetan chavan <chetan1792000@gmail.com>
2021-04-10 09:56:12 +0530

    adding_preference_and_setting

--- app/src/main/java/com/example/quakedetector/MainActivity.java
+++ app/src/main/java/com/example/quakedetector/MainActivity.java
@@ -7,11 +7,15 @@ import androidx.loader.content.Loader;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -21,9 +25,11 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.quakedetector.R.menu.menu_file;
+
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<LiveReportRow>>
 {
-    private static final String SAMPLE_JSON_RESPONSE = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=2&limit=20";
+    private static final String SAMPLE_JSON_RESPONSE = "https://earthquake.usgs.gov/fdsnws/event/1/query";
     private static final int EARTHQUAKE_LOADER_ID = 1;
     private TextView mEmptyTextView;
     private ProgressBar mProgressbar;
@@ -34,6 +40,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private CustomAdapter adapter;
     ListView earthquakeListView;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(menu_file,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            Intent settingsActivity = new Intent(this,SettingsActivity.class);
+            startActivity(settingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -83,8 +106,24 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public Loader<List<LiveReportRow>> onCreateLoader(int id, Bundle args)
     {
-        Log.e("LoaderMerits", "Inside the onCreateLoader");
-        return new EarthquakeLoader(this,SAMPLE_JSON_RESPONSE);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(SAMPLE_JSON_RESPONSE);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "20");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
     @Override
     public void onLoadFinished(@NonNull Loader<List<LiveReportRow>> loader, List<LiveReportRow> data) {
@@ -97,7 +136,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if (data != null && !data.isEmpty()) {
             adapter.addAll(data);
         }
-
     }
     @Override
     public void onLoaderReset(@NonNull Loader<List<LiveReportRow>> loader)
@@ -105,7 +143,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         Log.e("LoaderMerits", "Inside the onLoaderReset");
         adapter.clear();
     }
-
     //AsyncTask Implementation :
 /*    public class EarthQuakeAsynctask extends AsyncTask<String,Void, List<LiveReportRow>>
     {
